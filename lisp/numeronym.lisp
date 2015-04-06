a716e4bb8498bfd6ec8eceac86488a81b0acb778
masfj <masfj.pub@gmail.com>
2015-04-07 01:35:47 +0900

    2文字以下に対応

--- lisp/numeronym.lisp
+++ lisp/numeronym.lisp
@@ -1,7 +1,9 @@
 (defun numeronym (str)
   (and (stringp str)
        (let ((str-last-index (- (length str) 1)))
-        (concatenate 'string
-                     (subseq str 0 1)
-                     (princ-to-string (length (subseq str 1 str-last-index)))
-                     (subseq str str-last-index)))))
+         (if (<= str-last-index 1)
+             str
+             (concatenate 'string
+                          (subseq str 0 1)
+                          (princ-to-string (length (subseq str 1 str-last-index)))
+                          (subseq str str-last-index))))))
